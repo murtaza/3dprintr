3d3036c9bfede71928712f80bf01943c85f6365f
Murtaza Fatakdawala <murtazaaz98@gmail.com>
2019-11-22 12:20:21 -0500

    Added newlines in some places

--- motor.c
+++ motor.c
@@ -1,6 +1,7 @@
 #include "motor.h"
 
-void TEST_set_all_motor_high(){
+void TEST_set_all_motor_high()
+{
     int test = 0;
     GPIO_setOutputHighOnPin(MOTOR1_PORT1, MOTOR1_PIN1);
     GPIO_setOutputHighOnPin(MOTOR1_PORT2, MOTOR1_PIN2);
@@ -10,7 +11,7 @@ void TEST_set_all_motor_high(){
     GPIO_setOutputHighOnPin(MOTOR2_PORT2, MOTOR2_PIN2);
     GPIO_setOutputHighOnPin(MOTOR2_PORT3, MOTOR2_PIN3);
     GPIO_setOutputHighOnPin(MOTOR2_PORT4, MOTOR2_PIN4);
-    while(1)
+    while (1)
     {
         test++;
         displayScrollText("SPIN");
@@ -28,17 +29,21 @@ int move_x_motor(uint8_t quarterRotations, uint8_t dir, MOVE_DIRECTION moveDirec
     printString("MOTORS X MOVING \r\n");
 
     int i;
-    for (i = 0; i < quarterRotations; i++) {
+    for (i = 0; i < quarterRotations; i++)
+    {
 
-        if (edge_detect(moveDirection)){
+        if (edge_detect(moveDirection))
+        {
             printString("Abort: X-Edge Reached \r\n");
-            break;
+            // RIP we hit an edge
+            return 0;
         }
 
         counter = 0;
         while (counter <= COUNTER_MAX)
         {
-            switch (step) {
+            switch (step)
+            {
             case 0:
                 GPIO_setOutputLowOnPin(MOTOR1_PORT1, MOTOR1_PIN1);
                 GPIO_setOutputLowOnPin(MOTOR1_PORT2, MOTOR1_PIN2);
@@ -96,24 +101,31 @@ int move_x_motor(uint8_t quarterRotations, uint8_t dir, MOVE_DIRECTION moveDirec
             }
 
             sleep(sleep_time);
-            if (dir) { //Counter clockwise
+            if (dir)
+            { //Counter clockwise
                 step++;
-            } else {
+            }
+            else
+            {
                 step--;
             }
-            if (step > 7) step = 0;
-            if (step < 0) step = 7;
+            if (step > 7)
+                step = 0;
+            if (step < 0)
+                step = 7;
 
             counter++;
         }
 
-        if (dir == DIR_CLKWISE) {
+        if (dir == DIR_CLKWISE)
+        {
             curr_pos.x += 1;
-        } else {
+        }
+        else
+        {
             curr_pos.x -= 1;
         }
-
-        printPoint(curr_pos);
+        printCurrPos(curr_pos);
     }
 
     printString("MOTORS X DONE\r\n");
@@ -131,17 +143,21 @@ int move_y_motor(uint8_t quarterRotations, uint8_t dir, MOVE_DIRECTION moveDirec
     printString("MOTORS Y MOVING \r\n");
 
     int i;
-    for (i = 0; i < quarterRotations; i++) {
+    for (i = 0; i < quarterRotations; i++)
+    {
 
-        if (edge_detect(moveDirection)){
+        if (edge_detect(moveDirection))
+        {
             printString("Abort: Y-Edge Reached \r\n");
-            break;
+            // RIP we hit an edge.
+            return 0;
         }
 
         counter = 0;
         while (counter <= COUNTER_MAX)
         {
-            switch (step) {
+            switch (step)
+            {
             case 0:
                 GPIO_setOutputLowOnPin(MOTOR2_PORT1, MOTOR2_PIN1);
                 GPIO_setOutputLowOnPin(MOTOR2_PORT2, MOTOR2_PIN2);
@@ -199,24 +215,31 @@ int move_y_motor(uint8_t quarterRotations, uint8_t dir, MOVE_DIRECTION moveDirec
             }
 
             sleep(sleep_time);
-            if (dir) { //Counter clockwise
+            if (dir)
+            { //Counter clockwise
                 step++;
-            } else {
+            }
+            else
+            {
                 step--;
             }
-            if (step > 7) step = 0;
-            if (step < 0) step = 7;
+            if (step > 7)
+                step = 0;
+            if (step < 0)
+                step = 7;
 
             counter++;
         }
-        if (dir == DIR_CLKWISE) {
+        if (dir == DIR_CLKWISE)
+        {
             curr_pos.y += 1;
-        } else {
+        }
+        else
+        {
             curr_pos.y -= 1;
         }
 
-        printPoint(curr_pos);
-
+        printCurrPos(curr_pos);
     }
     printString("MOTORS Y DONE\r\n");
     return 1;
--- uart.c
+++ uart.c
@@ -3,25 +3,22 @@
 volatile char instructions[50];
 volatile uint8_t char_count = 0;
 
-
 volatile point points[50];
 volatile int num_points = 0;
 
-
-typedef enum State {
+typedef enum State
+{
     INIT,
     NOT_DONE,
     DONE,
     EXECUTE,
 } State;
 
-
 State state = INIT;
 
 /* EUSCI A0 UART ISR - Echoes data back to PC host */
-#pragma vector=USCI_A0_VECTOR
-__interrupt
-void EUSCIA0_ISR(void)
+#pragma vector = USCI_A0_VECTOR
+__interrupt void EUSCIA0_ISR(void)
 {
     uint8_t RxStatus = EUSCI_A_UART_getInterruptStatus(EUSCI_A0_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG);
 
@@ -36,7 +33,8 @@ void EUSCIA0_ISR(void)
             // Enter char
             state = DONE;
         }
-        else if (receiveData == 71){
+        else if (receiveData == 71)
+        {
             state = EXECUTE;
         }
         else
@@ -47,10 +45,13 @@ void EUSCIA0_ISR(void)
     }
 }
 
-int is_valid(char *point, uint8_t len){
+int is_valid(char *point, uint8_t len)
+{
     uint8_t i = 0;
-    while(point[i] != '\0') {
-        if (point[i] < '0' || point[i] > '9') {
+    while (point[i] != '\0')
+    {
+        if (point[i] < '0' || point[i] > '9')
+        {
             return 0;
         }
         i++;
@@ -58,26 +59,31 @@ int is_valid(char *point, uint8_t len){
     return 1;
 }
 
-
-void print_all_points() {
-    printString("Here are your points\n\r");
+void print_all_points()
+{
+    printString("\r\nHere are your points:\n\r");
 
     uint8_t i;
-    for (i = 0; i < num_points; i++) {
+    for (i = 0; i < num_points; i++)
+    {
         printPoint(points[i]);
         sleep(1500);
     }
 }
 
-void run_dont_stop(){
-    while(1){
-        switch(state) {
+void run_dont_stop()
+{
+    while (1)
+    {
+        switch (state)
+        {
         case INIT:
-            printString("Please enter your points: \r\n");
+            printString("Please enter your points (X Y): \r\n");
             state = NOT_DONE;
             break;
         case DONE:
-            if (parse_instructions() < 0 ) {
+            if (parse_instructions() < 0)
+            {
                 printString("Error: Invalid points given. Enter X Y \r\n");
             }
             char_count = 0;
@@ -98,7 +104,8 @@ void run_dont_stop(){
     }
 }
 
-int parse_instructions(){
+int parse_instructions()
+{
 
     uint8_t i = 0;
     char x[10];
@@ -106,28 +113,32 @@ int parse_instructions(){
     int x_len = 0;
     int y_len = 0;
     // Get x
-    while(instructions[i] != ' ') {
+    while (instructions[i] != ' ')
+    {
         x[x_len] = instructions[i];
         i++;
         x_len++;
     }
     x[x_len] = '\0';
     x_len++;
-    if (!is_valid(x, x_len)) {
+    if (!is_valid(x, x_len))
+    {
         return -1;
     }
 
     // Get y
     i++;
-    while ((i < char_count) && (instructions[i] != ' ')) {
+    while ((i < char_count) && (instructions[i] != ' '))
+    {
         y[y_len] = instructions[i];
         y_len++;
         i++;
     }
-    y[y_len] =  '\0';
+    y[y_len] = '\0';
     y_len++;
 
-    if (!is_valid(y, y_len)){
+    if (!is_valid(y, y_len))
+    {
         return -1;
     }
 
@@ -138,44 +149,74 @@ int parse_instructions(){
     return 0;
 }
 
-void execute_instructions() {
+void execute_instructions()
+{
     printString("Now executing instructions.\n\r");
     printString("Current position is: \n\r");
     printPoint(curr_pos);
     uint8_t i;
-    for (i = 0; i < num_points; i++) {
-        if (points[i].x > curr_pos.x) {
+    for (i = 0; i < num_points; i++)
+    {
+        if (points[i].x > curr_pos.x)
+        {
             // +x
-            if (!move_x_motor(points[i].x - curr_pos.x, DIR_CLKWISE, X_PLUS)){
-                printString("Edge detected (+X)");
+            if (!move_x_motor(points[i].x - curr_pos.x, DIR_CLKWISE, X_PLUS))
+            {
+                printString("Abort: Edge detected (+X) \r\n");
                 break;
             }
-        } else {
+        }
+        else
+        {
             // -x
-            if(!move_x_motor(curr_pos.x - points[i].x, DIR_COUNTER_CLKWISE, X_MINUS)){
-                printString("Edge detected (-X)");
+            if (!move_x_motor(curr_pos.x - points[i].x, DIR_COUNTER_CLKWISE, X_MINUS))
+            {
+                printString("Abort: Edge detected (-X) \r\n");
                 break;
             }
         }
 
-        if (points[i].y > curr_pos.y) {
+        if (points[i].y > curr_pos.y)
+        {
             // +y
-            if (!move_y_motor(points[i].y - curr_pos.y, DIR_CLKWISE, Y_PLUS)){
-                printString("Edge detected (+Y)");
+            if (!move_y_motor(points[i].y - curr_pos.y, DIR_CLKWISE, Y_PLUS))
+            {
+                printString("Abort: Edge detected (+Y) \r\n");
                 break;
             }
-        } else {
+        }
+        else
+        {
             // -y
-            if(!move_y_motor(curr_pos.y - points[i].y, DIR_COUNTER_CLKWISE, Y_MINUS)){
-                printString("Edge detected (-Y)");
+            if (!move_y_motor(curr_pos.y - points[i].y, DIR_COUNTER_CLKWISE, Y_MINUS))
+            {
+                printString("Abort: Edge detected (-Y) \r\n");
                 break;
             }
         }
     }
 }
 
-void printPoint(point p) {
+void printPoint(point p)
+{
     char dig_arr[10];
+
+    printString("X=");
+    seperateDigits(dig_arr, p.x);
+    printString(dig_arr);
+
+    printChar(' ');
+
+    printString("Y=");
+    seperateDigits(dig_arr, p.y);
+    printString(dig_arr);
+    printString("\n\r");
+}
+
+void printCurrPos(point p)
+{
+    char dig_arr[10];
+    printString("CurrPos: ");
 
     printString("X=");
     seperateDigits(dig_arr, p.x);
@@ -191,22 +232,25 @@ void printPoint(point p) {
 
 uint8_t countDigits(int num)
 {
-    uint8_t ret=1;
-    while (num/=10) ret++;
+    uint8_t ret = 1;
+    while (num /= 10)
+        ret++;
     return ret;
 }
 
-void seperateDigits(char *arr, uint8_t num) {
+void seperateDigits(char *arr, uint8_t num)
+{
     uint8_t digits = countDigits(num);
     uint8_t pos = digits;
-    while (pos--) {
+    while (pos--)
+    {
         arr[pos] = num % 10 + '0';
         num /= 10;
     }
     arr[digits] = '\0';
 }
 
-uint8_t stringToInt(char * string)
+uint8_t stringToInt(char *string)
 {
     return (uint8_t)atoi(string);
 }
